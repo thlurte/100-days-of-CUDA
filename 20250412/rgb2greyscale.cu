1eaab170a4ec2671e0a1be9dde15b54e1584eb24
thlurte <thlurte@gmail.com>
2025-04-13 12:11:09 +0530

    20250412 (day-02)

---
+++ 20250412/rgb2greyscale.cu
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+__global__ void rgb2GreyKernel(unsigned char* green, unsigned char* red, unsigned char* blue, unsigned int width, unsigned int height, unsigned char* grey){
+	;	
+}
+
+void rgb2Grey(unsigned char* red, unsigned char* green, unsigned char* blue, unsigned char* grey, unsigned int width, unsigned int height) {
+	cudaMallo
+	
+}
+
+
+int main() {
+
+	
+
+	return 0;
+}
